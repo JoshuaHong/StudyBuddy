c8d9a523819174a9c0b5cb77914b1bfd5afd60f9
Joshua Hong <Joshua99Hong@gmail.com>
2018-03-17 15:20:02 -0400

    Added dynamic buttons

--- StudyBuddy/app/src/main/java/com/example/josh/studybuddy/MainActivity.java
+++ StudyBuddy/app/src/main/java/com/example/josh/studybuddy/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        for (int x = 0; x < 3; x ++) {
+            Button button = new Button(this);
+            button.setText("This is the description sdfsdfsfd." + "\n" +
+                    "00:00 - " + "00:00" + "\n" + "2/3" + String.valueOf(x));
+            button.setId(x);
+            button.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(),"Dynamic button" + v.getId() + " is clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
+            linearLayout.addView(button);
+        }
+
+
+
+        scrollView.addView(linearLayout);
     }
 }
+
